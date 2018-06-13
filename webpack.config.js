66f4956d765eaf82bc63eb8a22c63029e4422776
Kevin Centeno <kevin@oldeyorke.ca>
2018-06-13 19:19:05 +0000

    Use webpack to build library

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var webpack = require('webpack');
+var path = require('path');
+var outputFile =  'language-tags.js';
+
+var config = {
+  entry: __dirname + '/lib/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: outputFile,
+    library: 'languageTags',
+  },
+};
+
+module.exports = config;
